cbbad46415b059c943fb0a3ed4c1d8160dc66fd6
Cassidy Noble <cub.noble@yahoo.fr>
2020-09-01 07:06:14 -0500

    Added a Ball Arc and Sound Effects
    Added a ball in a parabola with sound effects to Concourse Level

---
+++ Assets/BallInLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInLevel : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int minLifeTime, maxLifeTime;
+    float currentLifeLeft;
+
+    private void Start()
+    {
+        currentLifeLeft = Random.Range(minLifeTime, maxLifeTime);
+        this.transform.SetParent(GameObject.Find("Holder").transform);
+    }
+
+    private void Update()
+    {
+        currentLifeLeft -= Time.deltaTime;
+        if (currentLifeLeft <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void ActivateRB()
+    {
+        rb.useGravity = true;
+       // transform.parent = null;
+    }
+}
