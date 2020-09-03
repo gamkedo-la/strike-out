135d44d7069f0e0920a6aea3dfb21a66f6343fda
Cassidy Noble <cub.noble@yahoo.fr>
2020-09-03 06:58:46 -0500

    Adding to MiniMap
    Macroview and Miniview toggle with M

---
+++ Assets/AdjustMiniMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustMiniMap : MonoBehaviour
+{
+    public int yHeightBase, yHeightIncrease;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.M))
+        {
+            this.transform.localPosition = new Vector3(0, yHeightIncrease, 171);
+        }
+        else
+            this.transform.localPosition = new Vector3(0, yHeightBase, 0);
+    }
+}
