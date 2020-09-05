cc17f399d0b037f8e50de06c9ca9d6e8e63d2e61
Cassidy Noble <cub.noble@yahoo.fr>
2020-09-05 00:06:05 -0500

    Mini Map Cleanup. Mesh adjustment.
    Mini map cleanup and adjusting mesh to allow player to enter the field.

--- Assets/AdjustMiniMap.cs
+++ Assets/AdjustMiniMap.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class AdjustMiniMap : MonoBehaviour
 {
     public int yHeightBase, yHeightIncrease;
+    public GameObject map;
+    public float mapStart, mapExpand;
+    public Camera miniCam;
     void Update()
     {
         if (Input.GetKey(KeyCode.M))
         {
-            this.transform.localPosition = new Vector3(0, yHeightIncrease, 171);
+            this.transform.localPosition = new Vector3(50, yHeightIncrease, 171);
+            this.transform.rotation = Quaternion.Euler(90, 0, 0);
+            map.transform.localScale = new Vector2(mapExpand, mapExpand);
+            miniCam.orthographic = false;
+            miniCam.transform.parent = null;
         }
         else
+        {
             this.transform.localPosition = new Vector3(0, yHeightBase, 0);
+            map.transform.localScale = new Vector2(mapStart, mapStart);
+            miniCam.orthographic = true;
+            miniCam.transform.parent = GameObject.Find("Player").transform;
+        }
+
     }
 }
