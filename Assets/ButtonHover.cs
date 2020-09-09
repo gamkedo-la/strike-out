d55aa7cd041173aa848779d88b229c65e68d481f
Cassidy Noble <cub.noble@yahoo.fr>
2020-09-09 07:02:18 -0500

    Cont. Training Area - Shop Menu
    Continue work on the Training Area's shop menu. Minor and Major Scouting Report functionality is integrated.

--- Assets/ButtonHover.cs
+++ Assets/ButtonHover.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonHover : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class ButtonHover : MonoBehaviour
     public bool m1, m2, m3, m4, m5, m6, m7, m8;
     public bool M1, M2, M3, M4, M5, M6, M7, M8;
     public bool e1, e2, e3, e4, e5, e6, e7, e8;
+
+    public int price;
+    public Text buttonText;
     public void Enter()
     {
         Batter.SetActive(true);
@@ -21,55 +25,62 @@ public class ButtonHover : MonoBehaviour
 
     public void Purchase()
     {
-        if(m1)
-        { GameManager.m1 = true; }
-        if (m2)
-        { GameManager.m2 = true; }
-        if (m3)
-        { GameManager.m3 = true; }
-        if (m4)
-        { GameManager.m4 = true; }
-        if (m5)
-        { GameManager.m5 = true; }
-        if (m6)
-        { GameManager.m6 = true; }
-        if (m7)
-        { GameManager.m7 = true; }
-        if (m8)
-        { GameManager.m8 = true; }
+        if (price <= GameManager.Money)
+        {
+            GameManager.Money -= price;
 
-        if (M1)
-        { GameManager.M1 = true; }
-        if (M2)
-        { GameManager.M2 = true; }
-        if (M3)
-        { GameManager.M3 = true; }
-        if (M4)
-        { GameManager.M4 = true; }
-        if (M5)
-        { GameManager.M5 = true; }
-        if (M6)
-        { GameManager.M6 = true; }
-        if (M7)
-        { GameManager.M7 = true; }
-        if (M8)
-        { GameManager.M8 = true; }
+            if (m1)
+            { GameManager.m1 = true; }
+            if (m2)
+            { GameManager.m2 = true; }
+            if (m3)
+            { GameManager.m3 = true; }
+            if (m4)
+            { GameManager.m4 = true; }
+            if (m5)
+            { GameManager.m5 = true; }
+            if (m6)
+            { GameManager.m6 = true; }
+            if (m7)
+            { GameManager.m7 = true; }
+            if (m8)
+            { GameManager.m8 = true; }
 
-        if (e1)
-        { GameManager.e1 = true; }
-        if (e2)
-        { GameManager.e2 = true; }
-        if (e3)
-        { GameManager.e3 = true; }
-        if (e4)
-        { GameManager.e4 = true; }
-        if (e5)
-        { GameManager.e5 = true; }
-        if (e6)
-        { GameManager.e6 = true; }
-        if (e7)
-        { GameManager.e7 = true; }
-        if (e8)
-        { GameManager.e8 = true; }  
+            if (M1)
+            { GameManager.M1 = true; }
+            if (M2)
+            { GameManager.M2 = true; }
+            if (M3)
+            { GameManager.M3 = true; }
+            if (M4)
+            { GameManager.M4 = true; }
+            if (M5)
+            { GameManager.M5 = true; }
+            if (M6)
+            { GameManager.M6 = true; }
+            if (M7)
+            { GameManager.M7 = true; }
+            if (M8)
+            { GameManager.M8 = true; }
+
+            if (e1)
+            { GameManager.e1 = true; }
+            if (e2)
+            { GameManager.e2 = true; }
+            if (e3)
+            { GameManager.e3 = true; }
+            if (e4)
+            { GameManager.e4 = true; }
+            if (e5)
+            { GameManager.e5 = true; }
+            if (e6)
+            { GameManager.e6 = true; }
+            if (e7)
+            { GameManager.e7 = true; }
+            if (e8)
+            { GameManager.e8 = true; }
+            buttonText.text = "Purchased!";
+            this.GetComponent<Button>().interactable = false;
+        }
     }
 }
---
+++ Assets/ShopButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopButtons : MonoBehaviour
+{
+    public GameObject scoutingMenu, minorMenu, majorMenu;
+
+    public void Items()
+    {
+        scoutingMenu.SetActive(false);
+    }
+
+    public void ScoutingReport()
+    {
+        scoutingMenu.SetActive(true);
+        minorMenu.SetActive(true);
+    }
+
+    public void Minor()
+    {
+        minorMenu.SetActive(true);
+        majorMenu.SetActive(false);
+    }
+
+    public void Major()
+    {
+        minorMenu.SetActive(false);
+        majorMenu.SetActive(true);
+    }
+}
