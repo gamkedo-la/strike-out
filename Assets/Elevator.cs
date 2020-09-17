5983e721a0fa9c76129a70fec45023a7e87f534f
Cassidy Noble <cub.noble@yahoo.fr>
2020-09-17 06:49:18 -0500

    Started work on Elevator
    Elevator moves on trigger...but forgets the player.

---
+++ Assets/Elevator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public float bottomFloory, topFloory;
+    bool isTop;
+    public GameObject door;
+    public Vector3 doorOpen, doorClosed;
+    bool isMoving;
+
+    //Testing
+    public GameObject barrier;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isMoving = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(barrier);
+        }
+
+        if (isMoving)
+        {
+            door.transform.position = doorClosed;
+
+            if (isTop)
+            {
+                transform.Translate(Vector3.forward * -10 * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.forward * 10 * Time.deltaTime);
+            }
+        }
+
+        if (gameObject.transform.position.y >= topFloory)
+        {
+            isTop = true;
+            isMoving = false;
+            door.transform.position = doorOpen;
+        }
+
+        if (gameObject.transform.position.y <= bottomFloory)
+        {
+            isTop = false;
+            isMoving = false;
+            door.transform.position = doorOpen;
+        }
+
+        if (!isMoving)
+        {
+        }
+    }
+}
