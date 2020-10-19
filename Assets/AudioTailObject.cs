cea36b3877351b0ada5cc0a81cd4d89225bd70fe
rocleemusic <48lee@cardinalmail.cua.edu>
2020-10-18 23:11:58 -0400

    Setting Up sound assets for screen shatter

---
+++ Assets/AudioTailObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTailObject : MonoBehaviour
+{
+    public AudioSourceController controller;
+    public AudioData sound;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(this);   
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(this, sound.Clip.length);
+    }
+}
