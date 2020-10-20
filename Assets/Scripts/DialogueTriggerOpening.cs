5f45ba2a6b7662ca9ea6338ad0517eedface05d7
Cassidy Noble <cub.noble@yahoo.fr>
2020-10-20 16:24:46 -0500

    Intro Scene Created
    Intro scene created. Camera animations. Dialogue. Background colors. Flow into Training Area.

---
+++ Assets/Scripts/DialogueTriggerOpening.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriggerOpening : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public AudioEventGeneric dialogueBoxAudio;
+
+    private void Start()
+    {
+        StartCoroutine(Waiting());
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManagerOpening>().StartDialogue(dialogue);
+
+        if (dialogueBoxAudio != null)
+            dialogueBoxAudio.PlayEvent();
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(.25f);
+        TriggerDialogue();
+    }
+}
