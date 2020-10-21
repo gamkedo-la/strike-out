0e41d4c03dc14895c1942861d1939f5a00a6d861
Cassidy Noble <cub.noble@yahoo.fr>
2020-10-20 21:18:01 -0500

    MainMenu Fleshed out
    Main Menu characters, animations, menus, and lighting adjusted.

---
+++ Assets/introBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introBall : MonoBehaviour
+{
+    public GameObject menu, shatterball,ball;
+
+    public void EndOfAnim()
+    {
+        menu.SetActive(true);
+        shatterball.SetActive(true);
+        //ball.SetActive(false);
+    }
+}
---
+++ Assets/introPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introPlayer : MonoBehaviour
+{
+    public GameObject gloveBall, handball, animatedBall;
+
+    public void ToggleGloveHand()
+    {
+        gloveBall.SetActive(false);
+        handball.SetActive(true);
+    }
+
+    public void ToggleHandBall()
+    {
+        handball.SetActive(false);
+        animatedBall.SetActive(true);
+    }
+}
