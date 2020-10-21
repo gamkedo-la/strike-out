8bfaf709a893607300bb89abf6115ecdbe7b3901
Cassidy Noble <cub.noble@yahoo.fr>
2020-10-21 07:33:27 -0500

    Credits Page Update
    In Main Menu, credits page updated.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,9 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject menu, credits;
+    public Transform creditsSpot, main;
+    public GameObject cameraMain;
+    public GameObject shatterBall;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("IntroScene");
+    }
+
+    public void Credits()
+    {
+        cameraMain.transform.position = creditsSpot.transform.position;
+        cameraMain.transform.rotation = creditsSpot.transform.rotation;
+        shatterBall.SetActive(false);
+        menu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void Back()
+    {
+        cameraMain.transform.position = main.transform.position;
+        cameraMain.transform.rotation = main.transform.rotation;
+        shatterBall.SetActive(true);
+        menu.SetActive(true);
+        credits.SetActive(false);
     }
 
     public void Save()
---
+++ Assets/mainIntro.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainIntro : MonoBehaviour
+{
+    public GameObject cam;
+    public void AnimTurnOff()
+    {
+        cam.GetComponent<Animator>().enabled = false;
+    }
+   
+}
