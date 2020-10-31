06fb118b7bb4f5d95162197b100fb663bf6c253c
rocleemusic <48lee@cardinalmail.cua.edu>
2020-10-30 22:55:08 -0400

    ShatterBall Enemy sound

--- Assets/TurnObjectOn.cs
+++ Assets/TurnObjectOn.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TurnObjectOn : MonoBehaviour
 {
     public GameObject toTurnOn;
+    public AudioTailObject ShatterBallSound;
     // Start is called before the first frame update
     void Start()
     {
         toTurnOn.SetActive(true);
+
+        if (ShatterBallSound != null)
+            ShatterBallSound.PlaySoundWithTail();
     }
 }
