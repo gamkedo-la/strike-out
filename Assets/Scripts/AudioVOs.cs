79aeb74afa346373610fe41139ec047fa1510a0a
rocleemusic <48lee@cardinalmail.cua.edu>
2020-11-01 12:06:12 -0500

    add VO data to Music Prefab

--- Assets/Scripts/AudioVOs.cs
+++ Assets/Scripts/AudioVOs.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AudioVOs : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioData pitchVO;
+    public AudioData enemyAtkVO;
+    public AudioData playerTakeDmgVO;
+    public AudioData EnemyTakeDmgVO;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
